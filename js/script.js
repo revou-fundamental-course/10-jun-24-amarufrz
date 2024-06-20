8dc3300ab30b9c6528e7757354a91f585a98ccf3
amarufrz <amartyafaz@gmail.com>
2024-06-20 17:06:55 +0700

    akhir

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 function hapus(){
 document.getElementById('suhuc').value='';
-
+document.getElementById('suhuf').value='';
+document.getElementById('formula').value='';
 }
+
+
+
+function konversi(){
+    var result;
+    var celcius = document.getElementById('suhuc').value;
+    
+        result=(parseFloat(celcius)*9/5)+32;
+
+        document.getElementById('suhuf').value = parseFloat(result).toFixed(1) + '°F';
+        document.getElementById('formula').value = '( ' + parseFloat(celcius) +  '°C * 9/5 )' + ' + 32 = ' + parseFloat(result).toFixed(1) + '°F';
+}
+
+function balik(){
+    var result;
+    var celcius = document.getElementById('suhuc').value;
+    
+        result=(parseFloat(celcius)*9/5)+32;
+
+        document.getElementById('suhuf').value = parseFloat(result).toFixed(1)+'°F';
+        document.getElementById('formula').value = '( ' + parseFloat(result)+ '°F - 32 )' + ' *5/9 = ' + parseFloat(celcius)+'°C';
+}
+
+    
+    
